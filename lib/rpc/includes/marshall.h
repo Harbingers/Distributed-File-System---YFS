f0750d789e91d30c9f944503cfb41d2a2fb62ce3
Harbingers <financesmacro@gmail.com>
2016-04-06 15:41:50 -0400

    compile time min(), max()

---
+++ lib/rpc/includes/marshall.h
@@ -0,0 +1,16 @@
+#ifndef marshall_h
+#define marshall_h
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+#include <cstddef>
+
+#include "lang/verify.h"
+#include "lang/algorithm.h"
